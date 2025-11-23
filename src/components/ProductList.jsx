b44e6b1af097f65617b595aeaa52dd7b36189f28
wildanGhifari <wildghifari@gmail.com>
2025-11-23 11:35:14 +0000

    fix(frontend): Corrected CSS-in-JS syntax error in ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -113,7 +113,7 @@ function ProductList() {
               products.map((product) => (
                 <TableRow
                   key={product.id}
-                  sx={{ '&:last-child td, '&:last-child th': { border: 0 } }}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
                     {product.name}
